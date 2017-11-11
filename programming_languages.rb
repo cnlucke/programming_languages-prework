4cde93bfd2140d991675801c07f5897991e6fb9c
Cristy Lucke <cristy.lucke@gmail.com>
2017-11-11 22:17:14 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,8 +6,8 @@ def reformat_languages(languages)
     lang_style = style
     puts "LANG_STYLE: " + lang_style.to_s
     language.each do |name, attributes|
-      puts "EXISTS: #{new_hash[name].key?(:style)}"
       new_hash[name] = attributes
+      puts "EXISTS: #{new_hash[name].key?(:style)}"
       puts "NEW_HASH: #{new_hash}"
       puts
       #puts"new_hash[name][:style]: #{new_hash[name][:style]}"
